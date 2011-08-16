e446040fd17073b48f46d6c1f5d8a7151f453500
Simon A. Eugster <simon.eu@gmail.com>
2011-08-16 10:09:08 +0200

    Tarball script updated

--- build-tarball.sh
+++ build-tarball.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git archive master |bzip2 > slowmoVideo-$(./version.sh).tar.bz2
+# Creates a tarball when on a git repository. (git is required.)
+
+git archive master |bzip2 > slowmoVideo-sources-$(./version.sh).tar.bz2
