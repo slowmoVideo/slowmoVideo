0a4f6622f817fe29b0f4880ffe7a575852cb5969
valery <vbrasseur@gmail.com>
2015-11-09 11:39:35 +0100

    change the displpay message

--- src/slowmoVideo/project/videoFrameSource_sV.cpp
+++ src/slowmoVideo/project/videoFrameSource_sV.cpp
@@ -192,7 +192,7 @@ void VideoFrameSource_sV::locateFFmpeg()
                                   "\n(It is also possible that it took a little long to respond "
                                   "due to high workload, so you might want to try again.)"
                           #ifdef WINDOWS
-                                  "\nPlease download the 32-bit static ffmpeg build from ffmpeg.zeranoe.com "
+                                  "\nPlease download the static ffmpeg build from ffmpeg.zeranoe.com "
                                   "and extract ffmpeg.exe in the same directory as slowmoUI.exe."
                           #endif
                                   ));
