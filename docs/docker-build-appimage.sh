6491047551927b0b80c052288eceed476baf7b47
Simon A. Eugster <simon.eu@gmail.com>
2022-10-24 14:58:11 +0200

    Docker improvements

--- docs/docker-build-appimage.sh
+++ docs/docker-build-appimage.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 #
 # This file is intended to be run inside the Docker container.
+#
+# Expected volume mounts:
+# /slowmoVideo (Git repo)
+# /__build (build directory, AppImage is written to here too)
 
-# This script copies the built AppImage to /__build, so if the container is run with a volume mount on /__build,
-# the AppImage will be copied to the host.
-mkdir -p /__build
+# Ensure slowmoVideo exists
+if [ ! -e /slowmoVideo ]
+then
+  git clone https://github.com/slowmoVideo/slowmoVideo.git /slowmoVideo
+  cd /slowmoVideo || exit 1
+  git submodule update --init
+  cd ..
+fi
 
-mkdir /slowmoVideo/build
-cd /slowmoVideo/build
+# Ensure the build directory exists in the container
+if [ ! -e /__build ]
+then
+  mkdir -p /__build
+fi
+
+# Set the version from Git
+cd /slowmoVideo || exit 1
+pwd
+VERSION=$(git describe HEAD || exit 1)
+export VERSION
+echo "Building AppImage for version ${VERSION}"
+
+
+cd /__build || exit 1
 
 # Default steps for building the AppImage
 # Configure and build slowmoVideo
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake /slowmoVideo -DCMAKE_INSTALL_PREFIX=/usr
 make -j9
 
 # Install slowmoVideo to the AppDir directory for AppImage
@@ -20,15 +42,9 @@ make install DESTDIR=AppDir
 # Extract the linuxdeployqt AppImage when FUSE is not available in a docker container
 /linuxdeployqt.AppImage  --appimage-extract
 
-# Set the version from Git
-export VERSION=$(git describe HEAD)
-
 # Create the AppImage
 squashfs-root/AppRun AppDir/usr/share/applications/slowmoUI.desktop -appimage
 
 # Make the AppImage executable for everybody
 # It belongs to root, and without chmod only root can modify it outside of the container
 chmod 777 *.AppImage
-
-# Copy it to /__build where the volume mount might be
-cp *.AppImage /__build
